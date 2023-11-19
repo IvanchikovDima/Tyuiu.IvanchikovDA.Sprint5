c28d3ebf08e42eddbd4376acad9b83b14cfed020
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-11-19 22:50:01 +0500

    Task2V19

--- Tyuiu.IvanchikovDA.Sprint5.Task2.V19.Test/DataServiceTest.cs
+++ Tyuiu.IvanchikovDA.Sprint5.Task2.V19.Test/DataServiceTest.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Tyuiu.IvanchikovDA.Sprint5.Task1.V26.Lib;
+using Tyuiu.IvanchikovDA.Sprint5.Task2.V19.Lib;
 using System.IO;
 
-namespace Tyuiu.IvanchikovDA.Sprint5.Task1.V26.Test
+namespace Tyuiu.IvanchikovDA.Sprint5.Task2.V19.Test
 {
     [TestClass]
     public class DataServiceTest
@@ -11,13 +11,13 @@ namespace Tyuiu.IvanchikovDA.Sprint5.Task1.V26.Test
         [TestMethod]
         public void CheckedExistsFile()
         {
-            string path = @"C:\Users\User\source\repos\Tyuiu.IvanchikovDA.Sprint5\Tyuiu.IvanchikovDA.Sprint5.Task1.V26\bin\Debug\OutPutFileTask1.txt";
+            string path = @"C:\Users\User\source\repos\Tyuiu.IvanchikovDA.Sprint5\Tyuiu.IvanchikovDA.Sprint5.Task2.V19\bin\Debug\OutPutFileTask2.csv";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
-
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
+
         }
     }
 }
