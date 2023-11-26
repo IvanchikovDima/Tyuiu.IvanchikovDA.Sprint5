0656958b6f675da8479827f044c6a3965c8330f4
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-11-26 19:55:27 +0500

    Task4V30

---
+++ Tyuiu.IvanchikovDA.Sprint5.Task4.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanchikovDA.Sprint5.Task4.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanchikovDA.Sprint5.Task4.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bfeb9dd-2a5d-4264-9a6e-9c81103d88f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvanchikovDA.Sprint5.Task4.V30/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanchikovDA.Sprint5.Task4.V30.Lib;
+using System.IO;
+
+namespace Tyuiu.IvanchikovDA.Sprint5.Task4.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт#5 | Выполнил: Иванчиков Д.А. | ПКТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание 4                                                               *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Иванчиков Дмитрий Александрович | ПКТБ-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение по формуле                                           *");
+            Console.WriteLine("* и вернуть полученный результат на консоль.                              *");
+            Console.WriteLine("* Полученное значение округлить до трёх знаков после запятой              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V30.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
